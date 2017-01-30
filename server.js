95be1280a4938f071d1cf6ac78b2c433badc6063
protonsint <protonsint@gmail.com>
2017-01-31 00:05:26 +0530

    Update server.js
    updated port

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.configure(function(){
   app.use(express.static(__dirname + '/client'));
 });
 
-var port = process.env.PORT || 8080;
+//var port = process.env.PORT || 8080;
+var port = process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT  || 8080;
 app.listen(port, function() {
   console.log("Express server listening on port %d", app.address().port);
 });
